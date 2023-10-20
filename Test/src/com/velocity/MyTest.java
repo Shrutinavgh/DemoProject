74c855bda4b738dcea511c0d97fed1a2e57bbb21
Shruti <surupatil1999@gmail.com>
2023-10-21 00:41:31 +0530

    Added the new code

---
+++ Test/src/com/velocity/MyTest.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added the code to feature branch-01");
+	}
+
+}
