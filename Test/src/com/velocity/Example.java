8548769e9c282c0cb68c6c91eb1533fcce200d46
Shruti <surupatil1999@gmail.com>
2023-10-20 13:14:41 +0530

    Additon code is added

---
+++ Test/src/com/velocity/Example.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Addition is>>"+c);
+	}
+
+}
